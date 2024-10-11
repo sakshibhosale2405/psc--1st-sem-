6587661828c8d06861425e19e694132b5775f2ca
sakshibhosale2405 <sakshib2405@gmail.com>
2024-10-11 11:27:49 +0530

    Add files via upload

---
+++ sakshi1.py
@@ -0,0 +1,39 @@
+#print("hello, world")
+#this is a comment
+#number_of_chocolate=10
+#price_of_chocolate=5
+#print(number_of_chocolate*price_of_chocolate)
+#message = 10
+#print(message)
+#print(message)
+#avenger_assemble= "cap america"
+#print(avenger_assemble)
+#print("I am \n 'Batmaz'")
+#variable1 = 10
+#variable2 = 10.0
+#is_it = Truev
+#print(type(variable1))
+#print(type(variable2))
+#print(type(variable3))
+#print(type(is_it))
+#type casting
+#variable1="10"
+#print(int(variable1))
+#print(int("10"))
+#print(int("2.0"))
+#print(Float("10"))
+
+
+#number1= input()
+#print(number1)
+
+number1 = int(input("enter number1: "))
+number2= int(input("enter number2: "))
+
+print(number1+number2)
+
+
+
+
+
+
