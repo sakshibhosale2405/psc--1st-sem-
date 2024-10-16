8e5c471881cbdfa0ce4aea3455e331c4ddae06d7
sakshibhosale2405 <sakshib2405@gmail.com>
2024-10-16 16:44:48 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,35 @@
+#arithmetic operator
+print("arithmetic operator") 
+a=30
+b=23
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor diva=30
+b=23
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor division:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
+ision:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
+#regional operator
+x=16
+y=23
+print('x>yis',x>y)
+print('x<yis',x<y)
+print('x==yis',x==y)
+print('x!=yis',x!=y)
+print('x>=y is',x>=y)
+print('x<=yis',x<=y)
+
+#logical operator
+print("logical operator")
+print(10<5 and 10<20)
+print(10<5 or 10<20)
+print(not(10<20))
