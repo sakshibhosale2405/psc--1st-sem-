1e1d9ec65ac405ed55b52defef1758a6d7ce6d04
sakshibhosale2405 <sakshib2405@gmail.com>
2024-10-23 16:57:45 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,48 @@
+# temperature=int(input("enter temperature")) #temp que
+# if temperature<40:
+#     print("cold")
+# else:
+#     print("nice")
+
+   
+    
+
+# hours=int(input("enter total working hours"))#overtime working que
+# payroll=200
+# ob=12.5#ob=overtime bonous
+# if hours>40:
+#     b =hours*payroll*ob
+#     print("overtime pay",b)
+# else:
+#     a=hours*payroll
+#     print("usual pay",a) 
+
+# x=5
+# if x<10:
+#     print('smaller')
+# if x>20:
+#     print('Bigger')
+#  print finish
+      
+# x=5
+# if x==5 :
+#     print('equals 5')
+# if x>4 :
+#     print('greater than 4')
+# if x >=5 :
+#     print('greater than or equals 5')
+# if x<6 :
+#     print('less than 6')
+# if x<=5 :
+#     print('less than or equals 5')
+# if x !=6 :
+#     print('not equal 6')
+
+
+
+
+
+
+
+
+      
