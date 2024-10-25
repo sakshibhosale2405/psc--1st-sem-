834288e7f4a7caf75c30bcb9f603c20354a51e48
sakshibhosale2405 <sakshib2405@gmail.com>
2024-10-25 09:23:01 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,28 @@
+
+# a=12
+# last_digit=a%5
+# print(last_digit)
+
+# number= 123
+# if number%2 == 0:
+#    print("no is even",number)
+# else:
+#    print("no is odd",number)
+ 
+  
+
+# number=12345
+# if number%3 == 0:
+#     print("no is divisible by 3")
+# else:
+#     print ("no is not divisible by 3")  
+# if number%5 == 0:
+#     print("no is divisible by 5")  
+# else:
+#     print("no is not divisible by 5") 
+      
+number=9
+if number%3 == 0 number%5
+    print("no is divisible by 3")
+else:
+    print("no is not divisible by 3")    
