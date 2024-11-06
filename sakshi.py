b35cf158eeaba17b7657dc1788cc9c2ef8a5a31c
sakshibhosale2405 <sakshib2405@gmail.com>
2024-11-06 16:57:10 +0530

    Add files via upload
    loop

---
+++ sakshi.py
@@ -0,0 +1,23 @@
+# n=5
+# while n>0:
+#     print('lather')
+#     print(rinse)
+# print('blastoff!')
+# print(n)    
+
+#     line=input('>')
+#     if line [0] =='#' :
+#         continue
+#     if line == 'done'  :
+#         break
+#     print(line)
+# print('done')     
+sum=0
+i=1
+while i<=10:
+     sum += i
+     i += 1
+print('sum=',sum )
+
+
+   
