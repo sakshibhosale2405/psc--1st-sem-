03ce5804041449da1d0995aeb52f25cceac3ddc9
sakshibhosale2405 <sakshib2405@gmail.com>
2024-11-08 09:31:02 +0530

    Add files via upload

--- sakshi.py
+++ sakshi.py
@@ -1,23 +1,53 @@
-# n=5
-# while n>0:
-#     print('lather')
-#     print(rinse)
-# print('blastoff!')
-# print(n)    
+# print ("Happy Diwali")
+# number=int(input("enter a number: "))
+# if number%4 == 0: 
+#      print("yes")
+# elif number%5==0:
+#     print ("yes")  
+# else:
+#     print ("no")
+# word="happy diwali"
+# for _ in range(100):
+#     print (word)
+# i=0
+# i+= 1
+# for i in range(1,101):
+#     print(i)
+# i=1
+# i+=1
+# for i in range(1,21):
+#     num =i*i
+#     print(num)
 
-#     line=input('>')
-#     if line [0] =='#' :
-#         continue
-#     if line == 'done'  :
-#         break
-#     print(line)
-# print('done')     
-sum=0
-i=1
-while i<=10:
-     sum += i
-     i += 1
-print('sum=',sum )
+# def fibonacci_numbers(num):
+#     if num == 0:
+#         return 0
+#     elif num == 1:
+#         return 1
+#     else:
+#         return fibonacci_numbers(num-2)+fibonacci_numbers(num-1)
+ 
+ 
+# n = 11
+# for i in range(0, 11):
+    # print(fibonacci_numbers(i), end=" ")
+def fibonacci_numbers(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci_numbers(num-2)+fibonacci_numbers(num-1)
+ 
+ 
+n = 200
+for i in range(0, 200):
+    print(fibonacci_numbers(i), end=" ")
+      
+      
+      
 
 
-   
+
+    
+
