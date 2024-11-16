6d10292bd425ef80a896c530f4d752c1b72d5496
sakshibhosale2405 <sakshib2405@gmail.com>
2024-11-16 11:17:48 +0530

    Add files via upload

--- sakshi.py
+++ sakshi.py
@@ -1,53 +1,57 @@
-# print ("Happy Diwali")
-# number=int(input("enter a number: "))
-# if number%4 == 0: 
-#      print("yes")
-# elif number%5==0:
-#     print ("yes")  
-# else:
-#     print ("no")
-# word="happy diwali"
-# for _ in range(100):
-#     print (word)
-# i=0
-# i+= 1
-# for i in range(1,101):
-#     print(i)
-# i=1
-# i+=1
-# for i in range(1,21):
-#     num =i*i
-#     print(num)
 
-# def fibonacci_numbers(num):
-#     if num == 0:
-#         return 0
-#     elif num == 1:
-#         return 1
-#     else:
-#         return fibonacci_numbers(num-2)+fibonacci_numbers(num-1)
- 
- 
-# n = 11
-# for i in range(0, 11):
-    # print(fibonacci_numbers(i), end=" ")
-def fibonacci_numbers(num):
-    if num == 0:
-        return 0
-    elif num == 1:
-        return 1
-    else:
-        return fibonacci_numbers(num-2)+fibonacci_numbers(num-1)
- 
- 
-n = 200
-for i in range(0, 200):
-    print(fibonacci_numbers(i), end=" ")
-      
-      
-      
+# PSC Lab 16 Nov 2024 – Division C
+# Topic: Loop
+#que 1.
+# Write a program to calculate sum of first N natural numbers.
+
+# number,sum = 123456789,0
+# for i in range(number+1):
+#   sum+=i
+# print(sum)
+
+# que 2
+# Write a program to calculate sum of first N natural numbers without using any loop.
+
+# num=int(input("enter a number"))
+# sum=0
+# sum=num*(num+1)/2
+# print(sum)
+
+#  que 3
+# Write a program to calculate sum of even and odd numbers till N
+# n=int(input("enter a number"))
+# sum_even = 0
+# sum_odd = 0
+# for num in range(1, n+1):
+#  if num % 2 == 0:
+#   sum_even += num
+#  else:
+#   sum_odd += num
+# test1=int(input('enter score for test1:'))# print("Sum of even numbers:", sum_even)
+# print("Sum of odd numbers:", sum_odd)
 
 
+# que 4
+# Write a program to calculate sum of even and odd numbers till N without using any loop.
+# n=5
+# n_even=n//2#//used for floor value
+# n_odd=n//2
+# if n%2 == 1:
+#     n_odd= n_odd+1
+# sum_even=n_even*(n_even+1)
+# sum_odd=n_odd*n_odd
+# print("even sum:",sum_even)
+# print("odd sum:",sum_odd)    
 
-    
+
+     
+
+
+# que 5
+# Write a program to print the divisor of a positive number N.
+# n=int(input("Enter an integer:"))
+# print("The divisors of the number are:")
+# for i in range(1,n+1):
+#     if(n%i==0):
+#         print(i)
 
